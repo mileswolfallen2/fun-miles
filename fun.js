ecf5daee4ff2c350228bd7831afea07064086259
mileswa1q2 <115095719+mileswolfallen2@users.noreply.github.com>
2023-12-01 22:38:18 +0000

    idk

--- fun.js
+++ fun.js
@@ -1,3 +1,6 @@
 function Hello() {
     alert("Hi will stop adding thin on besember 2 for tow weeks and i will add things on beseber 16");
  }
+ function EventHandler() {
+    alert("you or gowind to a bifrint wed site");
+ }
