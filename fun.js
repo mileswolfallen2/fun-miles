59fbe700e89adee1d224f0e78534a28ee64ddef0
mileswa1q2 <115095719+mileswolfallen2@users.noreply.github.com>
2023-12-01 23:28:39 +0000

    idk

--- fun.js
+++ fun.js
@@ -2,5 +2,5 @@ function Hello() {
     alert("Hi will stop adding thin on besember 2 for tow weeks and i will add things on beseber 16");
  }
  function EventHandler() {
-    alert("you or gowind to a bifrint wed site");
+    alert("to play go to https://wondrous-jalebi-fb1e04.netlify.app/");
  }
