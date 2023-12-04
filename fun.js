ebe04ae5a69038bd624f631516c8075c625ad15c
mileswa1q2 <115095719+mileswolfallen2@users.noreply.github.com>
2023-12-04 17:39:41 +0000

    idk

--- fun.js
+++ fun.js
@@ -1,12 +1,12 @@
 function Hello() {
-    alert("Hi will stop adding thin on besember 2 for tow weeks and i will add things on beseber 16");
+    alert("Hi will stop adding thin on desember 2 for two weeks and i will add things on november 16");
  }
  function EventHandler() {
     alert("to play go to https://wondrous-jalebi-fb1e04.netlify.app/");
  }
  function getit() {
-alert("fun yesss🎅🏼 you fawnd santa congrats you won")
+alert("fun yesss🎅🏼 you found santa congrats you won")
  }
  function getinoroute(){
-   alert("fun-miles1.netlify.app is the new link if you or yoosing s old link it will not be updated ok by ps if you or on a git hud link you or still geting updats to got it you or fud")
+   alert("fun-miles1.netlify.app is the new link if you or useing the old link you will not be on the updated version if you are on a githud link you are still geting updates to it")
  }
