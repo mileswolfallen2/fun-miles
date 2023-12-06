6893402b164497c733fae990913c5372bb32e5ad
mileswa1q2 <115095719+mileswolfallen2@users.noreply.github.com>
2023-12-06 18:59:27 +0000

    idk

--- fun.js
+++ fun.js
@@ -1,15 +1,19 @@
 function Hello() {
     alert("Hi will stop adding thin on desember 2 for two weeks and i will add things on november 16");
  }
+ 
  function EventHandler() {
     alert("to play go to https://wondrous-jalebi-fb1e04.netlify.app/ ");
  }
- function idk(){
-   window.location.replace(https://wondrous-jalebi-fb1e04.netlify.app/)
+
+ function idk() {
+   window.location.replace (https://wondrous-jalebi-fb1e04.netlify.app/)
  }
+
  function getit() {
 alert("fun yesss🎅🏼 you found santa congrats you won")
  }
+
  function getinoroute(){
    alert("fun-miles1.netlify.app is the new link if you or useing the old link you will not be on the updated version if you are on a githud link you are still geting updates to it")
  }
