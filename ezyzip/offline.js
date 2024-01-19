418ce372aa2f229c4f469fa406cc3170302d1f57
mileswolfallen3 <147072155+mileswolfallen3@users.noreply.github.com>
2024-01-19 22:57:02 +0000

    idk

---
+++ ezyzip/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1705698535,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundtile.png",
+		"images/player-sheet0.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"images/nextlev-sheet0.png",
+		"images/lev-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
