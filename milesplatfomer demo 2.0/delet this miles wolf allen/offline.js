3d25ed38538be61137be44fac71957697953ada8
mileswolfallen3 <147072155+mileswolfallen3@users.noreply.github.com>
2024-01-22 23:36:17 +0000

    idk

---
+++ milesplatfomer demo 2.0/delet this miles wolf allen/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1705966050,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundtile.png",
+		"images/player-sheet0.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"images/nextlev-sheet0.png",
+		"images/lev-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/spritefont.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/backgroundtile2.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"media/for-the-game-i-em-make-sowned-made-with-clipchamp.m4a",
+		"media/untitled-video-made-with-clipchamp.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
