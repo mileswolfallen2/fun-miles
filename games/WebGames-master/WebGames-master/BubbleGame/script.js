a90f21187d43ae626c71abfdee3f8668dc9093dc
mileswa1q22 <115095719+mileswolfallen2@users.noreply.github.com>
2024-10-21 01:48:43 +0000

    this is cool

---
+++ games/WebGames-master/WebGames-master/BubbleGame/script.js
@@ -0,0 +1,135 @@
+
+gsap.from(".menu-screen .main-instruction",{
+    delay:0.2,
+    // x:-200,
+    // scale:0,
+    rotate:-45,
+    opacity:0,
+    duration: 0.3,
+})
+
+var panelContent = document.querySelector(".panel-content");
+var timerBox = document.querySelector(".timer-box");
+var hitBox = document.querySelector(".hit-box");
+var scoreBox = document.querySelector(".score-box");
+var gameOverScreen = document.querySelector(".game-over-screen");
+var gameOverScore = document.querySelector(".game-over-screen .current-score");
+var retplayBtn = document.querySelector(".game-over-screen button");
+var startBtn = document.querySelector(".menu-screen button");
+var highScoreDisplay = document.querySelector(".game-over-screen .high-score");
+
+var score = 0;
+
+function bubbleMaker(panelContent) {
+  var query = "";
+  var panelWidth = panelContent.offsetWidth;
+  var panelHeight = panelContent.offsetHeight;
+  var bubbleSize = 40;
+  var bubblePerRow = panelWidth / bubbleSize;
+  var numRow = panelHeight / bubbleSize;
+  var totalBubbles = bubblePerRow * numRow;
+
+  for (let i = 0; i <= totalBubbles; i++) {
+    var randomNum = Math.floor(Math.random() * 10);
+    query += ` <div class="bubble">${randomNum}</div> `;
+  }
+
+  document.querySelector(".panel-content").innerHTML = query;
+  var bubbles = document.getElementsByClassName("bubble");
+  for (let i = 0; i < bubbles.length; i++)
+  {
+      bubbles[i].style.animationName = "bubble-anim";
+      bubbles[i].style.animationIterationCount = "infinite";
+      bubbles[i].style.animationDelay = Math.random() + "s";
+      bubbles[i].style.animationDuration = 0.8 + Math.random()*0.5 + "s";
+  }
+}
+
+function gameOver() {
+    gameOverScreen.style.display = "flex";
+    gsap.from(".game-over-screen",{
+        y:"-100vh",
+        duration:0.3
+    });
+    gameOverScore.innerHTML = score;
+    if (score > getHighScore())
+    {
+        setHighScore(score);
+    }
+    highScoreDisplay.innerHTML = getHighScore();
+}
+
+function setHighScore(score) {
+    document.cookie = "highScore=" + score + ";path=/";
+}
+function getHighScore() {
+    if (document.cookie == "") {
+        return 0;
+    } else {
+        return document.cookie.split("=")[1];
+    }
+}
+
+var time = 60;
+timerBox.innerHTML = time;
+function timeFunction() {
+    var timerInterval = setInterval(() => {
+    if (time > 0) {
+      time--;
+      timerBox.innerHTML = time;
+    }
+    else{
+        gameOver();
+        clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
+function generateHit() {
+    let hitNum = Math.floor(Math.random()*10);
+    hitBox.innerHTML = hitNum;
+}
+
+panelContent.addEventListener("click", (dets)=>{//Event bubbling
+    if(dets.target.classList.contains('bubble')) {
+        if(dets.target.innerHTML == hitBox.innerHTML){
+            score += 10;
+            scoreBox.innerHTML = score;
+            if (score >= 0) {
+                scoreBox.style.color = "rgb(28, 76, 223)";
+            }
+            generateHit();
+            bubbleMaker(panelContent);
+        }
+        else{
+            score -= 10;
+            scoreBox.innerHTML = score;
+            if (score < 0) {
+                scoreBox.style.color = "red";
+            }
+            generateHit();
+            bubbleMaker(panelContent);
+        }
+    }
+});
+
+startBtn.addEventListener("click", () => {
+
+    gsap.to(".menu-screen",{
+        scale:0
+    });
+    generateHit();
+    timeFunction();
+    bubbleMaker(panelContent);
+});
+
+retplayBtn.addEventListener("click", () => {
+    score = 0;
+    scoreBox.innerHTML = score;
+    scoreBox.style.color = "rgb(28, 76, 223)";
+    gameOverScreen.style.display = "none"
+    generateHit();
+    timeFunction();
+    bubbleMaker(panelContent);
+    time = 61;
+});
---
+++ games/WebGames-master/WebGames-master/script.js
@@ -0,0 +1,97 @@
+const mainContent = document.querySelector(".main-content");
+const navBar = document.querySelector(".nav-bar");
+const gameText = document.getElementsByClassName("game-text");
+const navItem = document.getElementsByClassName("nav-item");
+const gameIcon = document.getElementsByClassName("game");
+const sideBar = document.getElementById("side-bar-menu");
+const darkModeIcon = document.getElementById("dark-mode-icon");
+const sidebarDarkModeIcon = document.getElementById("sidebar-dark-mode-icon");
+
+const onToggleDarkMode = () => {
+  if (darkModeIcon.classList.contains("fa-moon")) {
+    mainContent.style.backgroundColor = "#15202B";
+    sideBar.style.backgroundColor = "rgb(1, 125, 63)";
+    for (let i = 0; i < gameText.length; i++) {
+      gameText[i].style.color = "#E4E6EB";
+    }
+    for (let i = 0; i < gameIcon.length; i++) {
+      gameIcon[i].style.setProperty("--game-icon-hover-color", "#22303C");
+    }
+
+    navBar.style.backgroundColor = "#03DAC5";
+    navBar.style.color = "#212628";
+    for (let i = 0; i < navItem.length; i++) {
+      navItem[i].style.color = "#212628";
+    }
+    darkModeIcon.className = "fa-solid fa-sun";
+    sidebarDarkModeIcon.className = "fa-solid fa-sun";
+  } else {
+    mainContent.style.backgroundColor = "white";
+    sideBar.style.backgroundColor = "rgb(0, 144, 72)";
+    for (let i = 0; i < gameText.length; i++) {
+      gameText[i].style.color = "#333333";
+    }
+    for (let i = 0; i < gameIcon.length; i++) {
+      gameIcon[i].style.setProperty("--game-icon-hover-color", "#dedede");
+    }
+
+    navBar.style.backgroundColor = "#009090";
+    navBar.style.color = "white";
+    for (let i = 0; i < navItem.length; i++) {
+      navItem[i].style.color = "white";
+    }
+    darkModeIcon.className = "fa-solid fa-moon";
+    sidebarDarkModeIcon.className = "fa-solid fa-moon";
+  }
+};
+
+const toggleSideBar = () => {
+  const currentWidth = sideBar.style.width;
+  if (currentWidth === "73vw") {
+    sideBar.style.width = "0";
+    mainContent.style.filter = "brightness(1)";
+  } else {
+    sideBar.style.width = "73vw";
+    mainContent.style.filter = "brightness(50%)";
+  }
+};
+
+// Rendering the main content dynamically
+const mainContentData = [
+  {
+    name: "Tic-Tac-Toe",
+    image: "assets/tic-tac-toe.png",
+    link: "tic-tac-toe/index.html",
+  },
+  {
+    name: "The Bubble Game",
+    image: "assets/the_bubble_game.png",
+    link: "BubbleGame/index.html",
+  },
+  {
+    name: "Dev Quiz",
+    image: "assets/dev_quiz.png",
+    link: "DevQuiz/index.html",
+  },
+  {
+    name: "HangMan",
+    image: "assets/hangman.png",
+    link: "https://hangmangame02.netlify.app/",
+  },
+  {
+    name: "Tic-Tac-Toe",
+    image: "assets/tic-tac-toe.png",
+    link: "https://fun-miles-4jub.onrender.com/test",
+  },
+];
+
+mainContentData.forEach(item => {
+  let newDiv = `
+     <a href=${item.link} class="game game1" target="_blank">
+        <img src=${item.image} alt="" />
+        <h3 class="game-text">${item.name}</h3>
+      </a>
+  `;
+
+  mainContent.innerHTML += newDiv;
+});
