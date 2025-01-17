80f7318c5e3502de2e7db9899da5ee32a32fc819
Robert <152218269+PenguinRobert1@users.noreply.github.com>
2025-01-17 16:13:06 +0000

    1

--- WebGames-master/WebGames-master/script.js
+++ WebGames-master/WebGames-master/script.js
@@ -231,7 +231,7 @@ const mainContentData = [
   },
   {
     name: "2048",
-    image: "/",
+    image: "/image-robert-added/Screenshot 2025-01-17 10.11.56 AM.png",
     link: "/dao-2048-main/dao-2048-main/index.html",
   },
 ];
