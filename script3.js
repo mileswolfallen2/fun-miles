074ec6cec9cc06456f4fe23b4aaccca7ad419306
Robert <152218269+PenguinRobert1@users.noreply.github.com>
2025-02-14 17:15:49 +0000

    1

--- script3.js
+++ script3.js
@@ -85,6 +85,11 @@ const mainContentData = [
     link: "/WebGames-master/WebGames-master/pokemon-netplay/pokemon/sapphire/index.html",
   },
   {
+    name: "Pokemon Pearl",
+    image: "/WebGames-master/WebGames-master/assets/61p+1+PYSML.jpg",
+    link: "/WebGames-master/WebGames-master/pokemon-netplay/pokemon/sapphire/index.html",
+  },
+  {
     name: "Cat",
     image: "/image-robert-added/pokefun.png",
     link: "/cat.html",
