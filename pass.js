53cc07dc30a36e712aff7176cde42434fa01ab8c
Robert <152218269+PenguinRobert1@users.noreply.github.com>
2025-03-10 18:01:51 +0000

    yes

--- pass.js
+++ pass.js
@@ -25,11 +25,12 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+
 const text = document.querySelector('input.password.visible');
 const pass = document.querySelector('input.password.hidden');
 const handle = document.querySelector('img.handle');
 
-text.addEventListener('input', (e) => {
+text.addEventListener('input', (e)=>{
   pass.value = e.target.value;
 });
 
@@ -37,30 +38,30 @@ let startingPos;
 let paneUp = true;
 let angle;
 
-handle.addEventListener('mousedown', (e) => {
+handle.addEventListener('mousedown', (e)=>{
   startingPos = e.clientY;
   document.body.classList.add('grabbing');
-});
+})
 
-document.addEventListener('mousemove', (e) => {
+document.addEventListener('mousemove', (e)=>{
   if (startingPos) {
     if (paneUp) {
-      if (e.clientY > startingPos && e.clientY < startingPos + 160) {
-        angle = ((e.clientY - startingPos) * 180) / 160;
+      if (e.clientY > startingPos && e.clientY < startingPos+160) {
+        angle = ((e.clientY-startingPos) * 180) / 160;
         pass.style.transform = `rotateX(${angle}deg)`;
         handle.style.transform = `translateY(-40px) rotateX(${angle}deg)`;
       }
     } else {
-      if (e.clientY < startingPos && e.clientY > startingPos - 160) {
-        angle = 360 + ((e.clientY - startingPos - 160) * 180) / 160;
+      if (e.clientY < startingPos && e.clientY > startingPos-160) {
+        angle = 360 + ((e.clientY-startingPos-160) * 180) / 160;
         pass.style.transform = `rotateX(${angle}deg)`;
         handle.style.transform = `translateY(-40px) rotateX(${angle}deg)`;
       }
     }
   }
-});
+})
 
-document.addEventListener('mouseup', (e) => {
+document.addEventListener('mouseup', (e)=>{
   document.body.classList.remove('grabbing');
   if (startingPos) {
     if (angle >= 90) {
@@ -75,4 +76,4 @@ document.addEventListener('mouseup', (e) => {
   }
 
   startingPos = undefined;
-});
+})
